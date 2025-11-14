22b3aad4b6b120c584b23929c7cbe1e4dfd05f88
scriptedvestige <contact@scriptedvestige.com>
2025-11-13 19:15:11 -0800

    Add manual_run method to CleanUp class
    Added manual_run method to allow manual execution of the cleaner.  Added weather output directories targeted.

--- cleaner.py
+++ cleaner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from utils.time_utils import filename_format, day_name, time_of_day
-from utils.file_utils import alerts_out, news_out, wardrobe_out
+from utils.file_utils import alerts_out, news_out, wardrobe_out, weather_out
 import os
 
 
@@ -14,7 +14,7 @@ class CleanUp():
         # Date
         self.today = day_name()
         # Paths
-        self.all_dirs = [alerts_out(), news_out(), wardrobe_out()]
+        self.all_dirs = [alerts_out(), news_out(), wardrobe_out(), weather_out()]
 
     def run(self):
         """Run the cleaner."""
@@ -34,8 +34,25 @@ class CleanUp():
                         full_path = os.path.join(dir, file)
                         if os.path.exists(full_path):
                             os.remove(full_path)
-        
+
+    def manual_run(self):
+        """Manually run the cleaner."""
+        for dir in self.all_dirs:
+            # Grab all filenames in directory.
+            all_files = os.listdir(dir)
+            for file in all_files:
+                """
+                Get the file date from the file name.
+                Doing it this way is simpler because the date is in the name so it requires no imports or time format conversions.
+                """
+                file_date = file.split("_")[-1][:-5]
+                # If file date is older than current date, delete the file after ensuring it exists.
+                if file_date != filename_format():
+                    full_path = os.path.join(dir, file)
+                    if os.path.exists(full_path):
+                        os.remove(full_path)
+
 
 if __name__ == "__main__":
     janitor = CleanUp()
-    janitor.run()
+    janitor.manual_run()
